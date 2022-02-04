fef134e178ef6722613ed86b2f848229048eec25
scottieboyzhang <97482103+scottieboyzhang@users.noreply.github.com>
2022-02-04 15:38:15 -0800

    add Test and junit

--- Mt.java
+++ Mt.java
@@ -1,1 +1,5 @@
-public class Mt{}
+public class Mt{
+    public static int division(int a, int b){
+        return a / b;
+    }
+}
---
+++ MtTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MtTest {
+    @Test 
+    public void divisionTest(){
+        assertEquals(1, Mt.division(4, 1));
+    }
+}
